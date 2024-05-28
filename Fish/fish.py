1219458742137dd88750bc784288009ffb18ab7b
UseG1GC <160996117+UseG1GC@users.noreply.github.com>
2024-05-28 15:00:25 +1000

    Update fish.py

--- Fish/fish.py
+++ Fish/fish.py
@@ -18,3 +18,25 @@ class Fish:
         dict = response.json()
         reply = dict["message"]["content"]
         return reply
+
+class Judge:
+    def __init__(self, url, sysmessage="You are an AI fish. You will be given a scenario and what a player tries to do, and you will come up with a result.", limit = 20):
+        self.url = url
+        self.sysmessage = sysmessage
+        self.messages = [{"role": "system","content": f"{self.sysmessage}"}]
+        self.limit = limit
+
+    def generate(self, payload):
+        self.messages.append({"role": "user","content": f"{payload}"})
+        data = {
+            "model": "Fish:1.5",
+            "messages": self.messages,
+            "stream": False
+        }
+        response = requests.post(self.url, json=data)
+        dict = response.json()
+        reply = dict["message"]["content"]
+        self.messages.append(dict["message"])
+        if len(self.messages) > self.limit:
+            del self.messages[1:2]
+        return reply
