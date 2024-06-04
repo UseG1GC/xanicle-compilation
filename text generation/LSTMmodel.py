e0d372f6793969a26668dd17f2e7b6c4f6dd3e90
UseG1GC <160996117+UseG1GC@users.noreply.github.com>
2024-06-05 09:56:16 +1000

    update to use actual tokenizer

--- text generation/LSTMmodel.py
+++ text generation/LSTMmodel.py
@@ -4,54 +4,50 @@ import pandas as pd
 from torch import nn, optim
 from torch.utils.data import DataLoader
 from collections import Counter
+import tokenmonster
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, sequence_length=4):
+    def __init__(self, sequence_length=32):
+        self.vocab = tokenmonster.load("english-24000-consistent-v1")
         self.sequence_length=sequence_length
         self.words = self.load_words()
-        self.vocab = self.load_vocab()
-
-        self.index_to_word = {index: word for index, word in enumerate(self.vocab)}
-        self.word_to_index = {word: index for index, word in enumerate(self.vocab)}
-
-        self.words_indexes = [self.word_to_index[w] for w in self.words]
 
     def load_words(self):
-        data = pd.read_parquet("data/data.parquet")
-        text = data["text"].str.cat(sep=" ")
-        return text.split(" ")
-        
-    def load_vocab(self):
-        word_counts = Counter(self.words)
-        return sorted(word_counts, key=word_counts.get, reverse=True)
+        output = []
+        data = pd.read_parquet(f"data/data.parquet")
+        for chunk in data["text"]:
+            if len(chunk) > 0:
+                for i in self.vocab.tokenize(chunk):
+                    output.append(int(i))
+        return output
     
     def __len__(self):
-        return len(self.words_indexes) - self.sequence_length
+        return len(self.words) - self.sequence_length
 
     def __getitem__(self, i):
         return(
-            torch.tensor(self.words_indexes[i:i+self.sequence_length]),
-            torch.tensor(self.words_indexes[i+1:i+self.sequence_length+1]),
+            torch.tensor(self.words[i:i+self.sequence_length]),
+            torch.tensor(self.words[i+1:i+self.sequence_length+1]),
         )
 
 class LSTMModel(nn.Module):
-    def __init__(self, dataset, model_path="model.pth"):
+    def __init__(self, dataset, model_path="LSTMmodel.pth"):
         super(LSTMModel, self).__init__()
-        self.n_layers = 6
-        self.n_lstm = 128
-        self.embedding_dim = 128
+        self.n_layers = 16
+        self.n_lstm = 512
         self.dropout = 0.2
-        n_vocab = len(dataset.vocab)
-
-        self.embedding = nn.Embedding(num_embeddings=n_vocab,embedding_dim=self.embedding_dim)
+        self.n_vocab = len(dataset.vocab)
+        self.embedding = nn.Embedding(num_embeddings=self.n_vocab,embedding_dim=self.n_lstm)
         self.lstm = nn.LSTM(input_size=self.n_lstm,hidden_size=self.n_lstm,num_layers=self.n_layers,dropout=self.dropout)
-        self.fc = nn.Linear(self.n_lstm,n_vocab)
+        self.fc = nn.Linear(self.n_lstm,self.n_vocab)
         self.model_path = model_path
+
         try:
             self.load_state_dict(torch.load(model_path))
             print(f"Model loaded at location {model_path}")
         except:
-            print("Initalizing new model")
+            print("Model not found, initializing new model")
+    
     def forward(self, x, prev_state):
         y = self.embedding(x)
         output, state = self.lstm(y, prev_state)
@@ -61,37 +57,45 @@ class LSTMModel(nn.Module):
     def _init_state(self, seq_length):
         return(torch.zeros(self.n_layers,seq_length,self.n_lstm),torch.zeros(self.n_layers,seq_length,self.n_lstm))
     
-def train_loop(dataset,model,batch_size=1024,seq_length=4,max_epochs=50):
+def train_loop(dataset,initial_model,batch_size=1024,seq_length=32,max_epochs=50):
+    model = torch.compile(initial_model)
+
     model.train()
 
     train_data = DataLoader(dataset,batch_size=batch_size)
+    lr = 1e-3
 
     loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-    for epoch in range(max_epochs):
-        a, b = model._init_state(seq_length)
-        total_loss = 0
-        average_loss = 0
+    try:
+        for epoch in range(max_epochs):
+            optimizer = optim.Adam(model.parameters(), lr=lr)
+            a, b = model._init_state(seq_length)
+            total_loss = 0
+            average_loss = 0
 
-        for batch, (x, y) in enumerate(train_data):
-            optimizer.zero_grad()
+            for batch, (x, y) in enumerate(train_data):
+                optimizer.zero_grad()
 
-            y_prediction, (a, b) = model(x, (a,b))
-            loss = loss_fn(y_prediction.transpose(1,2), y)
+                y_prediction, (a, b) = model(x, (a,b))
+                loss = loss_fn(y_prediction.transpose(1,2), y)
 
-            a = a.detach()
-            b = b.detach()
+                a = a.detach()
+                b = b.detach()
 
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(),0.25)
-            optimizer.step()
-            total_loss += loss.item()
-            average_loss = total_loss / (batch+1)
-            torch.save(model.state_dict(),model.model_path)
-            print(f"Epoch: {epoch}, batch: {batch}, loss: {loss.item()}")
-        
-        print(f"Epoch : {epoch}, avg_loss : {average_loss}")
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(),0.25)
+                optimizer.step()
+                total_loss += loss.item()
+                average_loss = total_loss / (batch+1)
+                print(f"Epoch: {epoch}, batch: {batch}, loss: {loss.item()}")
+            
+            print(f"Epoch : {epoch}, avg_loss : {average_loss}")
+            lr = lr / 10
+    except KeyboardInterrupt:
+        initial_model.eval()
+        torch.save(initial_model.state_dict(),initial_model.model_path)
+        print("Model Saved!")
 
 def generate(dataset,model,input_text,n_words):
     text = input_text.split(" ")
--- text generation/modeltrain.py
+++ text generation/modeltrain.py
@@ -1,6 +1,8 @@
 from LSTMmodel import *
 
-dataset = Dataset()
+seq_length = 32
+
+dataset = Dataset(sequence_length=seq_length)
 model = LSTMModel(dataset=dataset)
 
-train_loop(dataset,model,seq_length=4,batch_size=8192,max_epochs=100)
+train_loop(dataset,model,seq_length=seq_length,batch_size=1024,max_epochs=100)
