bf84250382009f99116cafd91b0b23733f1fe1f3
UseG1GC <160996117+UseG1GC@users.noreply.github.com>
2024-06-05 10:14:10 +1000

    Update LSTMgenerate.py

--- text generation/LSTMgenerate.py
+++ text generation/LSTMgenerate.py
@@ -3,8 +3,8 @@ from LSTMmodel import *
 dataset = Dataset()
 model = LSTMModel(dataset=dataset)
 
-n_words = input("Enter amount of words to autocomplete: ")
+n_words = int(input("Enter amount of words to autocomplete: "))
+
 
 while True:
-    for chunk in generate(dataset,model,input_text=input("\nEnter Text: "),n_words=n_words):
-        print(chunk, end=" ")
+    print(generate(dataset,model,input_text=input("Enter Text: "),n_words=n_words))
